049cbfaa8c7fdf7ac9b994dc57091471d88a7a92
RadiationX <radiationx@yandex.ru>
2022-06-15 20:05:30 +0500

    feat: add splashscreen

--- example/src/main/java/ru/mintrocket/mintpermissions/MainActivity.kt
+++ example/src/main/java/ru/mintrocket/mintpermissions/MainActivity.kt
@@ -2,6 +2,7 @@ package ru.mintrocket.mintpermissions
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import by.kirich1409.viewbindingdelegate.viewBinding
 import ru.mintrocket.mintpermissions.common.Routes
 import ru.mintrocket.mintpermissions.databinding.ActivityMainBinding
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private val binding by viewBinding<ActivityMainBinding>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
 
         binding.btDialogs.setOnClickListener {
