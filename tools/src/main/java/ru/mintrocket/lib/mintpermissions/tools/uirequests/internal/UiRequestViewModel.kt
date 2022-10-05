eff814c39bfcefe73fe92af206d0aacbbe22d696
Obolrom <obolrom@gmail.com>
2022-10-05 20:28:26 +0300

    revert: reverted commit with removing nullable job

--- tools/src/main/java/ru/mintrocket/lib/mintpermissions/tools/uirequests/internal/UiRequestViewModel.kt
+++ tools/src/main/java/ru/mintrocket/lib/mintpermissions/tools/uirequests/internal/UiRequestViewModel.kt
@@ -3,10 +3,11 @@ package ru.mintrocket.lib.mintpermissions.tools.uirequests.internal
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import ru.mintrocket.lib.mintpermissions.tools.uirequests.UiRequestConfig
 import ru.mintrocket.lib.mintpermissions.tools.uirequests.models.UiRequest
 import ru.mintrocket.lib.mintpermissions.tools.uirequests.models.UiResult
@@ -22,9 +23,7 @@ internal class UiRequestViewModel<T, R>(
     }
 
     private val queue by lazy { FlowQueue<UiRequest<T>>() }
-    private val observingNewScope by lazy {
-        CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
-    }
+    private var observingNewJob: Job? = null
 
     val requestFlow = queue.headFlow
 
@@ -44,11 +43,6 @@ internal class UiRequestViewModel<T, R>(
             .launchIn(viewModelScope)
     }
 
-    override fun onCleared() {
-        observingNewScope.cancel(CancellationException("UiRequestViewModel is cleared"))
-        super.onCleared()
-    }
-
     fun finishRequest(result: UiResult<T, R>) {
         viewModelScope.launch {
             queue.remove(result.request)
@@ -66,14 +60,15 @@ internal class UiRequestViewModel<T, R>(
 
     private fun startObservingNew() {
         stopObservingNew()
-        controller
+        observingNewJob = controller
             .observeNewRequest()
             .onEach(queue::add)
             .onEach(controller::consumeRequest)
-            .launchIn(observingNewScope)
+            .launchIn(viewModelScope)
     }
 
     private fun stopObservingNew() {
-        observingNewScope.coroutineContext.job.cancelChildren()
+        observingNewJob?.cancel()
+        observingNewJob = null
     }
 }
